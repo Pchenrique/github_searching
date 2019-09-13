8055546614c192dda71c1e6f680f5f71e0c173c1
Pchenrique <pchenrique1998@hotmail.com>
2019-09-13 19:07:03 -0300

    Puxando a bio do perfil do github

--- AjaxSync.js
+++ AjaxSync.js
@@ -4,12 +4,14 @@ const btn = document.querySelector("#buscar-avatar");
 const getFotoGithub = function () {
     let username = document.querySelector("#username").value;
     var url = 'https://api.github.com/users/' + username;
+    let bio = document.querySelector("#bio");
 
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
 			let pesquisa = JSON.parse(this.responseText);
                 imagem.src = pesquisa.avatar_url;
+                bio.innerHTML = pesquisa.bio;
             }	
 	}
     ajax.open('GET', url, true);
